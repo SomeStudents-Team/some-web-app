aa4d0297d5b04c805ec9a07ea9691b635f1a3596
Mazunin Ivan <mazuninim@gmail.com>
2022-12-28 20:35:55 +0500

    Добавлена заготовка для верстки приложения

--- src/http-ui/src/App.tsx
+++ src/http-ui/src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {AppProps} from "./models/app/AppProps";
+import {AppStates} from "./models/app/AppStates";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Navbar from "./components/Navbar";
+import {History} from "./pages/History";
+import Main from "./pages/Main";
+import Container from "@mui/material/Container";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component<AppProps, AppStates> {
+
+  render() {
+    return <BrowserRouter>
+
+      <Navbar />
+
+      <Container fixed sx={{
+        marginTop: '5%'
+      }}>
+
+        <Routes>
+          <Route element={<Main />} path="/" />
+          <Route element={<History />} path="/history" />
+        </Routes>
+
+      </Container>
+
+    </BrowserRouter>
+  }
 }
 
 export default App;
